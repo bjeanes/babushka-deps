d3e30ff0e61184b50e26c026eacc94b0e5408e00
Bo Jeanes <me@bjeanes.com>
2013-12-03 09:07:52 -0800

    Link personal deps to source deps

--- system.rb
+++ system.rb
@@ -1,6 +1,7 @@
 dep 'system' do
   requires \
     'ssh key',
+    'set up personal deps',
     'dot files',
     'packages',
     'rubies'
@@ -9,6 +10,18 @@ end
 dep 'ssh key', :key do
   key.default! '~/.ssh/id_rsa'.p.expand
 
-  met? { key.p.exists? }  
+  met? { key.p.exists? }
   meet { shell "ssh-keygen -t rsa -b 4096  -f #{key.to_s.inspect}" }
+end
+
+dep 'set up personal deps' do
+  user = ENV['USER']
+  source_name = File.basename(File.dirname(__FILE__))
+  personal = '~/.babushka/deps'.p
+
+  met? {
+    user != source_name || personal.exists?
+  }
+
+  meet { shell 'ln', '-s', File.dirname(__FILE__), personal.to_s }
 end
