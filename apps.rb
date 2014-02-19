2d05ae8b3cf983b547a7e74909b91748d55f79c5
Bo Jeanes <me@bjeanes.com>
2014-02-18 19:22:20 -0800

    adium accounts and prefs

--- apps.rb
+++ apps.rb
@@ -8,10 +8,45 @@ end
 dep 'osx apps' do
   requires %w[
     Alfred.app Dropbox.app MacVim.app 1Password.app
-    Google\ Chrome.app Adium.app Skype.app iTerm.app
+    Google\ Chrome.app adium Skype.app iTerm.app
     iStat\ Menus.app Bartender.app HipChat.app
   ]
 end
+
+dep 'adium' do
+  requires 'Adium.app', 'adium prefs'
+end
+
+dep 'adium prefs' do
+  requires 'adium accounts', 'disable adium sounds'
+end
+
+dep 'disable adium sounds' do
+  file = '~/Library/Application Support/Adium 2.0/Users/Default/Contact Alerts.plist'.p
+  met? { !file.exists? }
+  meet { file.remove }
+end
+
+dep 'adium accounts' do
+  plist_buddy = '/usr/libexec/PlistBuddy'
+  file = '~/Library/Application Support/Adium 2.0/Users/Default/Accounts.plist'.p
+  plist_apply = ->(cmd) { shell plist_buddy, '-c', cmd, file }
+
+  met? { file.grep(/me@bjeanes.com/) }
+  meet {
+    file.parent.mkdir
+    plist_apply.("Clear dict")
+    plist_apply.("Add :Accounts array")
+    plist_apply.("Add :Accounts:0 dict")
+    plist_apply.("Add :Accounts:0:ObjectID string 1")
+    plist_apply.("Add :Accounts:0:Service string GTalk")
+    plist_apply.("Add :Accounts:0:Type string libpurple-jabber-gtalk")
+    plist_apply.("Add :Accounts:0:UID string me@bjeanes.com")
+
+    shell "open", "https://accounts.google.com/b/0/IssuedAuthSubTokens"
+  }
+end
+
 
 dep 'HipChat.app' do
   source 'https://www.hipchat.com/downloads/latest/mac'
